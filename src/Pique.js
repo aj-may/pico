97f8cbd97cefa2bbdf90dd7584e890267a0d3daa
A.J. May <aj7may@gmail.com>
2018-12-15 15:29:24 -0500

    (feature) Hats 🎅

--- src/Pique.js
+++ src/Pique.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PiqueText from './PiqueText';
 import PiqueAudio from './PiqueAudio';
+import santaHat from './santaHat.svg';
 
 const Pique = ({ classes, pique }) => (
   <div className={classes.pique}>
     {pique.type === 'text' && <PiqueText pique={pique} />}
     {pique.type === 'audio' && <PiqueAudio pique={pique} />}
+    <img src={santaHat} className={classes.hat} alt="" />
   </div>);
 
 const styles = theme => ({
@@ -14,6 +16,13 @@ const styles = theme => ({
     display: 'inline-flex',
     marginTop: theme.spacing.unit,
     marginRight: theme.spacing.unit,
+    position: 'relative',
+  },
+  hat: {
+    height: '1.2rem',
+    position: 'absolute',
+    top: '-.5rem',
+    left: '-.5rem',
   }
 });
 
